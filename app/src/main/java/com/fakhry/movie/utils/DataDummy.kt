8fd091a7f742178d8cf108297abf56b4249bc858
Fakhry Mubarak <fakhrymubarak@gmail.com>
2020-11-16 23:11:31 +0800

    Add data dummy tvShow

--- app/src/main/java/com/fakhry/movie/utils/DataDummy.kt
+++ app/src/main/java/com/fakhry/movie/utils/DataDummy.kt
@@ -65,7 +65,8 @@ object DataDummy {
         val rating = doubleArrayOf(6.0, 1.0, 5.0, 7.0, 8.3, 5.9, 7.3, 6.3, 7.7, 6.5, 6.4)
 
         val movieList = ArrayList<MovieAndTvShowEntity>()
-        for (i in movieId.indices){
+
+        for (i in movieId.indices) {
             val movie = MovieAndTvShowEntity()
             movie.id = movieId[i]
             movie.title = movieTitle[i]
@@ -77,5 +78,80 @@ object DataDummy {
         }
 
         return movieList
+    }
+
+    fun generateDummyTvShow(): ArrayList<MovieAndTvShowEntity> {
+        val tvShowId = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+        val tvShowTitle = arrayOf(
+            "The Good Doctor",
+            "The Mandalorian",
+            "Fear the Walking Dead",
+            "Lucifer",
+            "The Boys",
+            "The Queen's Gambit",
+            "The Walking Dead: World Beyond",
+            "His Dark Materials",
+            "Grey's Anatomy",
+            "The Simpsons",
+        )
+
+        val tvShowSynopsis = arrayOf(
+            "A young surgeon with Savant syndrome is recruited into the surgical unit of a prestigious hospital. The question will arise: can a person who doesn't have the ability to relate to people actually save their lives?",
+            "After the fall of the Galactic Empire, lawlessness has spread throughout the galaxy. A lone gunfighter makes his way through the outer reaches, earning his keep as a bounty hunter.",
+            "What did the world look like as it was transforming into the horrifying apocalypse depicted in \"The Walking Dead\"? This spin-off set in Los Angeles, following new characters as they face the beginning of the end of the world, will answer that question.",
+            "Bored and unhappy as the Lord of Hell, Lucifer Morningstar abandoned his throne and retired to Los Angeles, where he has teamed up with LAPD detective Chloe Decker to take down criminals. But the longer he's away from the underworld, the greater the threat that the worst of humanity could escape.",
+            "A group of vigilantes known informally as “The Boys” set out to take down corrupt superheroes with no more than blue-collar grit and a willingness to fight dirty.",
+            "In a Kentucky orphanage in the 1950s, a young girl discovers an astonishing talent for chess while struggling with addiction.",
+            "A heroic group of teens sheltered from the dangers of the post-apocalyptic world receive a message that inspires them to leave the safety of the only home they have ever known and embark on a cross-country journey to find the one man who can possibly save the world.",
+            "Lyra is an orphan who lives in a parallel universe in which science, theology and magic are entwined. Lyra's search for a kidnapped friend uncovers a sinister plot involving stolen children, and turns into a quest to understand a mysterious phenomenon called Dust. She is later joined on her journey by Will, a boy who possesses a knife that can cut windows between worlds. As Lyra learns the truth about her parents and her prophesied destiny, the two young people are caught up in a war against celestial powers that ranges across many worlds.",
+            "Follows the personal and professional lives of a group of doctors at Seattle’s Grey Sloan Memorial Hospital.",
+            "Set in Springfield, the average American town, the show focuses on the antics and everyday adventures of the Simpson family; Homer, Marge, Bart, Lisa and Maggie, as well as a virtual cast of thousands. Since the beginning, the series has been a pop culture icon, attracting hundreds of celebrities to guest star. The show has also made name for itself in its fearless satirical take on politics, media and American life in general."
+       )
+
+        val tvShowPosterUrl = arrayOf(
+            "/6tfT03sGp9k4c0J3dypjrI8TSAI.jpg",
+            "/sWgBv7LV2PRoQgkxwlibdGXKz1S.jpg",
+            "/wGFUewXPeMErCe2xnCmmLEiHOGh.jpg",
+            "/4EYPN5mVIhKLfxGruy7Dy41dTVn.jpg",
+            "/mY7SeH4HFFxW1hiI6cWuwCRKptN.jpg",
+            "/zU0htwkhNvBQdVSIKB9s6hgVeFK.jpg",
+            "/z31GxpVgDsFAF4paZR8PRsiP16D.jpg",
+            "/g6tIKGc3f1H5QMz1dcgCwADKpZ7.jpg",
+            "/clnyhPqj1SNgpAdeSS6a6fwE6Bo.jpg",
+            "/2IWouZK4gkgHhJa3oyYuSWfSqbG.jpg"
+        )
+
+        val backgroundPath = arrayOf(
+            "/iDbIEpCM9nhoayUDTwqFL1iVwzb.jpg",
+            "/9ijMGlJKqcslswWUzTEwScm82Gs.jpg",
+            "/58PON1OrnBiX6CqEHgeWKVwrCn6.jpg",
+            "/ta5oblpMlEcIPIS2YGcq9XEkWK2.jpg",
+            "/mGVrXeIjyecj6TKmwPVpHlscEmw.jpg",
+            "/34OGjFEbHj0E3lE2w0iTUVq0CBz.jpg",
+            "/pLVrN9B750ehwTFdQ6n3HRUERLd.jpg",
+            "/9yKCJTOh9m3Lol2RY3kw99QPH6x.jpg",
+            "/3IIBf6VlwEyKAX4cN2XCM1gKdgM.jpg",
+            "/hpU2cHC9tk90hswCFEpf5AtbqoL.jpg"
+
+        )
+
+        val rating = doubleArrayOf(6.0, 1.0, 5.0, 7.0, 8.3, 5.9, 7.3, 6.3, 7.7, 6.5, 6.4)
+
+
+        val tvShowList = ArrayList<MovieAndTvShowEntity>()
+
+        for (i in tvShowId.indices) {
+            val tvShow = MovieAndTvShowEntity()
+            tvShow.id = tvShowId[i]
+            tvShow.title = tvShowTitle[i]
+            tvShow.synopsis = tvShowSynopsis[i]
+            tvShow.poster_url = tvShowPosterUrl[i]
+            tvShow.backdrop_url = backgroundPath[i]
+            tvShow.rating = rating[i]
+            tvShowList.add(tvShow)
+        }
+
+        return tvShowList
     }
 }
